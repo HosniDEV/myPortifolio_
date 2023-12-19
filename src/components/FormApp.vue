9b755032efa6142e2e762c4252af5aeb5a31208b
Hosni_Cr <105177254+HosniDEV@users.noreply.github.com>
2023-12-19 13:29:51 +0100

    formApp updated

--- src/components/FormApp.vue
+++ src/components/FormApp.vue
@@ -4,7 +4,14 @@
       <div class="bg-primary" style="height: 1px; width: 50%"></div>
       <div><h1 class="text-lg font-medium mb-2 text-end">Hit me up</h1></div>
     </div>
-    <form action="" class="w-full space-y-2" @submit.prevent="">
+    <form action="" class="w-full space-y-2" @submit.prevent="sendEmail()">
+      <!-- flash Message -->
+      <div
+        class="bg-green-500 text-center text-base font-bold py-1 rounded-md transition-all ease-out"
+        v-if="flashMessage"
+      >
+        Email sent successfully
+      </div>
       <div class="relative">
         <span v-if="cancel" class="absolute right-2 inset-y-0 flex items-center"
           ><svg
@@ -25,42 +32,81 @@
         </span>
         <input
           type="text"
-          placeholder="your email address"
+          placeholder="your  name"
           class="w-full px-2 py-2 focus:outline-none focus:ring focus:ring-primary rounded-md placeholder:text-sm bg-black border border-primary"
           required
-          v-model="email"
+          v-model="name"
           @keypress="email != ' ' ? (cancel = true) : (cancel = false)"
         />
       </div>
+
       <div>
         <input
-          type="password"
-          placeholder="............"
+          type="email"
+          placeholder="your email address"
+          v-model="email"
           class="w-full px-2 py-2 focus:outline-none focus:ring focus:ring-primary rounded-md placeholder:text-sm bg-black border border-primary"
+          required
         />
       </div>
-      <input
-        type="submit"
-        placeholder="............"
-        value="Submit"
-        class="w-full px-2 py-2 focus:outline-none focus:ring focus:ring-primary rounded-md bg-primary font-bold"
+
+      <textarea
+        placeholder="your message "
+        id="message"
+        v-model="message"
+        class="w-full px-2 py-2 focus:outline-none focus:ring focus:ring-primary rounded-md placeholder:text-sm bg-black border border-primary"
         required
-      />
+      ></textarea>
+
+      <button
+        class="w-full px-2 py-2 focus:outline-none focus:ring focus:ring-primary rounded-md bg-primary font-bold"
+      >
+        submit
+      </button>
     </form>
   </div>
 </template>
 
 <script>
+import emailjs from "emailjs-com";
 export default {
   data() {
     return {
       cancel: false,
-      email: "",
+      name: "abdellah",
+      email: "hosni@gmail.com",
+      message: "well done",
+      flashMessage: false,
     };
   },
-  watch: {
-    email(val) {
-      console.log(val);
+
+  methods: {
+    sendEmail() {
+      const templateParams = {
+        from_name: this.name,
+        from_email: this.email,
+        message: this.message,
+      };
+
+      // Your EmailJS userID and service ID
+      const userID = "lMqlWvU3tn2YxJ0C7";
+
+      const templateID = "template_wxzd8nk";
+      const serviceID = "service_0097";
+
+      emailjs
+        .send(serviceID, templateID, templateParams, userID)
+        .then((response) => {
+          this.flashMessage = true;
+          setTimeout(() => {
+            this.flashMessage = false;
+          }, 2000);
+          console.log("Email sent!", response);
+          // Reset form fields after successful submission
+          this.name = "";
+          this.email = "";
+          this.message = "";
+        });
     },
   },
 };
