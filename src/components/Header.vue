562b3f4160e6b6438a4bfde78e2af29e3eda15fc
Hosni_Cr <105177254+HosniDEV@users.noreply.github.com>
2023-12-19 13:58:54 +0100

    add an icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,18 +4,37 @@
       ><div>
         <img src="/images/logo.png" alt="" width="40" /></div
     ></router-link>
-    <router-link to="skills"
-      ><div
-        class="text-base font-thin text-primary border border-primary px-3 py-1 rounded-md shadow-sm shadow-primary hover:bg-third hover:text-white transition-colors duration-500 ease-out"
+    <router-link to="skills">
+      <div
+        class="text-base font-thin text-primary border border-primary px-3 py-1 rounded-md shadow-sm shadow-primary hover:bg-third hover:text-white transition-colors duration-500 ease-out group"
       >
-        <h1>ABDELLAH HOSNI</h1>
-      </div></router-link
-    >
+        <h1>
+          ABDELLAH HOSNI
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            data-slot="icon"
+            class="w-5 h-5 text-primary animate-bounce group-hover:text-white inline"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+            />
+          </svg>
+        </h1></div
+    ></router-link>
   </div>
 </template>
 
 <script>
-export default {};
+import { ChevronDoubleDownIcon } from "@vue-hero-icons/outline";
+export default {
+  components: { ChevronDoubleDownIcon },
+};
 </script>
 
 <style lang="scss" scoped></style>
